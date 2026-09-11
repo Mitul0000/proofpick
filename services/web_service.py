aaa112b5d4d7d426c9626f59e2b7a5d9e02b0342
Mitul Chowdhury <mitulchowdhury042006@gmail.com>
2026-09-11 07:22:57 +0530

    Added code structure for web service

---
+++ services/web_service.py
@@ -0,0 +1,24 @@
+from config.settings import Setting
+
+settings = Setting()
+
+class WebExtractor:
+
+    def __init__(self):
+        pass
+
+    def search_on_web(self):
+            pass
+
+    
+    def web_link_filter(self):
+        pass
+
+
+    def webpage_content_extractor(self):
+        pass
+
+    def webcontent_filter(self):
+        pass
+
+    
