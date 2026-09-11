f2fb7d7e6d3e4fef778158a1f2171040a71e1abe
Mitul Chowdhury <mitulchowdhury042006@gmail.com>
2026-09-11 07:25:15 +0530

    Updated Setting

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,14 @@
 from pydantic_settings import BaseSettings
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 class Setting(BaseSettings):
-    gemini_api_key:str
+    groq_api_key: str = os.getenv('GROQ_API_KEY')
     min_views_for_youtube:int = 10000
     max_youtube_results:int = 10
+    min_youtube_views:int = 10000
+    min_youtube_subscriber:int = 10000
+    min_duration:int = 600
